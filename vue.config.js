6c63b2b40fb1dab7628dfbb4bc1ea20d3cb8877e
ariadnagrimau <149061076+ariadnagrimau@users.noreply.github.com>
2023-12-21 10:35:48 +0100

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+
+/*const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+  
+})
+*/
+
+module.exports ={
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/prova5/' : '/'
+}
+
+
+    
